8acfe94de19ea86e7baea33a47b99b67d453a890
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-13 09:29:55 +0300

    Add files via upload

---
+++ halloween-game/scripts/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 500
+
+func _physics_process(_delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
