be4b1aa134eced12138a5618da9060f29e25863e
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-14 09:15:38 +0300

    Now new pumpkin spawns after pickup

--- halloween-game/scripts/game_manager.gd
+++ halloween-game/scripts/game_manager.gd
@@ -3,6 +3,13 @@ extends Node2D
 @export var pumpkin_scene: PackedScene
 
 func _ready() -> void:
+	spawn_pumpkin()
+
+func _process(_delta: float) -> void:
+	if !get_node(".").has_node("pumpkin"):
+		spawn_pumpkin()
+
+func spawn_pumpkin() -> void:
 	var pumpkin = pumpkin_scene.instantiate()
 	
 	var pumpkin_spawn_location = $Path2D/PathFollow2D
