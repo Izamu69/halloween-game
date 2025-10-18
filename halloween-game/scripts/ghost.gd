80615562f92bfdd7d8217645fcc1db052492d9b3
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-18 17:21:34 +0300

    Ghosts now restart game on collision with player, added sprite for player light

--- halloween-game/scripts/ghost.gd
+++ halloween-game/scripts/ghost.gd
@@ -18,3 +18,12 @@ func _physics_process(_delta: float) -> void:
 		$AnimatedSprite2D.flip_h = false
 	
 	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		var tree = get_tree()
+		if tree:
+			call_deferred("_restart_scene")
+		
+func _restart_scene():
+	get_tree().reload_current_scene()
