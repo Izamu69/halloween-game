9886b652edb0997c6e763325992ae4adc626e046
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-22 09:46:16 +0300

    Improved on ghost spawning, added 1 room for each part of the map

--- halloween-game/scripts/game_manager.gd
+++ halloween-game/scripts/game_manager.gd
@@ -3,12 +3,23 @@ extends Node2D
 @export var pumpkin_scene: PackedScene
 @export var ghost_scene: PackedScene
 @export var top_right_scene: PackedScene
-var spawn_points
+@export var middle_left_scene: PackedScene
+@export var middle_right_scene: PackedScene
+@export var bottom_right_scene: PackedScene
+@export var top_left_scene: PackedScene
+@export var bottom_left_scene: PackedScene
+var pumpkin_spawn_points
+var ghost_spawn_points
 
 func _ready() -> void:
 	spawn_top_right()
-	spawn_points = get_tree().get_nodes_in_group("pumpkin_spawns")
-	print(spawn_points)
+	spawn_top_left()
+	spawn_middle_left()
+	spawn_middle_right()
+	spawn_bottom_right()
+	spawn_bottom_left()
+	pumpkin_spawn_points = get_tree().get_nodes_in_group("pumpkin_spawns")
+	ghost_spawn_points = get_tree().get_nodes_in_group("ghost_spawns")
 	spawn_pumpkin()
 	spawn_ghost()
 	spawn_ghost()
@@ -19,10 +30,10 @@ func _process(_delta: float) -> void:
 
 var last_pumpkin_position = null
 func spawn_pumpkin() -> void:
-	if spawn_points.is_empty():
+	if pumpkin_spawn_points.is_empty():
 		return
 	
-	var available_spawns = spawn_points.duplicate()
+	var available_spawns = pumpkin_spawn_points.duplicate()
 	if last_pumpkin_position and available_spawns.size() > 1:
 		available_spawns.erase(last_pumpkin_position)
 
@@ -33,16 +44,42 @@ func spawn_pumpkin() -> void:
 	pumpkin.position = spawn.global_position
 	add_child(pumpkin)
 
+var last_ghost_position
 func spawn_ghost() -> void:
+	if ghost_spawn_points.is_empty():
+		return
+	
+	var available_spawns = ghost_spawn_points.duplicate()
+	if last_ghost_position and available_spawns.size() > 1:
+		available_spawns.erase(last_ghost_position)
+
+	var spawn = available_spawns[randi() % available_spawns.size()]
+	last_ghost_position = spawn
+	
 	var ghost = ghost_scene.instantiate()
-	
-	var ghost_spawn_location = $Path2D/PathFollow2D
-	ghost_spawn_location.progress_ratio = randf()
-	
-	ghost.position = ghost_spawn_location.position
-	
+	ghost.position = spawn.global_position
 	add_child(ghost)
 
 func spawn_top_right() -> void:
 	var top_right = top_right_scene.instantiate()
 	add_child(top_right)
+	
+func spawn_top_left() -> void:
+	var top_left = top_left_scene.instantiate()
+	add_child(top_left)
+
+func spawn_middle_left() -> void:
+	var middle_left = middle_left_scene.instantiate()
+	add_child(middle_left)
+
+func spawn_middle_right() -> void:
+	var middle_right = middle_right_scene.instantiate()
+	add_child(middle_right)
+
+func spawn_bottom_right() -> void:
+	var bottom_right = bottom_right_scene.instantiate()
+	add_child(bottom_right)
+
+func spawn_bottom_left() -> void:
+	var bottom_left = bottom_left_scene.instantiate()
+	add_child(bottom_left)
