de678545406160ef5e382c5d3f8ef86517d0123c
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-22 22:36:53 +0300

    Added all of the rooms, made them generate randomly

--- halloween-game/scripts/game_manager.gd
+++ halloween-game/scripts/game_manager.gd
@@ -2,12 +2,25 @@ extends Node2D
 
 @export var pumpkin_scene: PackedScene
 @export var ghost_scene: PackedScene
+
 @export var top_right_scene: PackedScene
+@export var top_right_scene2: PackedScene
+
 @export var middle_left_scene: PackedScene
+@export var middle_left_scene2: PackedScene
+
 @export var middle_right_scene: PackedScene
+@export var middle_right_scene2: PackedScene
+
 @export var bottom_right_scene: PackedScene
+@export var bottom_right_scene2: PackedScene
+
 @export var top_left_scene: PackedScene
+@export var top_left_scene2: PackedScene
+
 @export var bottom_left_scene: PackedScene
+@export var bottom_left_scene2: PackedScene
+
 var pumpkin_spawn_points
 var ghost_spawn_points
 
@@ -61,25 +74,37 @@ func spawn_ghost() -> void:
 	add_child(ghost)
 
 func spawn_top_right() -> void:
-	var top_right = top_right_scene.instantiate()
+	var top_right_scenes = [top_right_scene, top_right_scene2]
+	var scene = top_right_scenes[randi() % top_right_scenes.size()]
+	var top_right = scene.instantiate()
 	add_child(top_right)
 	
 func spawn_top_left() -> void:
-	var top_left = top_left_scene.instantiate()
+	var top_left_scenes = [top_left_scene, top_left_scene2]
+	var scene = top_left_scenes[randi() % top_left_scenes.size()]
+	var top_left = scene.instantiate()
 	add_child(top_left)
 
 func spawn_middle_left() -> void:
-	var middle_left = middle_left_scene.instantiate()
+	var middle_left_scenes = [middle_left_scene, middle_left_scene2]
+	var scene = middle_left_scenes[randi() % middle_left_scenes.size()]
+	var middle_left = scene.instantiate()
 	add_child(middle_left)
 
 func spawn_middle_right() -> void:
-	var middle_right = middle_right_scene.instantiate()
+	var middle_right_scenes = [middle_right_scene, middle_right_scene2]
+	var scene = middle_right_scenes[randi() % middle_right_scenes.size()]
+	var middle_right = scene.instantiate()
 	add_child(middle_right)
 
 func spawn_bottom_right() -> void:
-	var bottom_right = bottom_right_scene.instantiate()
+	var bottom_right_scenes = [bottom_right_scene, bottom_right_scene2]
+	var scene = bottom_right_scenes[randi() % bottom_right_scenes.size()]
+	var bottom_right = scene.instantiate()
 	add_child(bottom_right)
 
 func spawn_bottom_left() -> void:
-	var bottom_left = bottom_left_scene.instantiate()
+	var bottom_left_scenes = [bottom_left_scene, bottom_left_scene2]
+	var scene = bottom_left_scenes[randi() % bottom_left_scenes.size()]
+	var bottom_left = scene.instantiate()
 	add_child(bottom_left)
