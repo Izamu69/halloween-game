54df928a2a2c8aac2d9ff8acdb12f7e066778941
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-23 22:38:38 +0300

    Added main menu and game over screens

--- halloween-game/scripts/game_over.gd
+++ halloween-game/scripts/game_over.gd
@@ -1,8 +1,10 @@
 extends Control
 
 func _ready() -> void:
-	$Label.text = "Score: " + str(PumpkinCount.pumpkin_count)
+	$VBoxContainer/Label.text = "Score: " + str(PumpkinCount.pumpkin_count)
 
 func _input(_event):
-	if Input.is_key_pressed(KEY_X):
+	if Input.is_key_pressed(KEY_R):
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
+	if Input.is_key_pressed(KEY_Q):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
---
+++ halloween-game/scripts/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready() -> void:
+	$VBoxContainer/Label2.text = "High score: " + str(PumpkinCount.pumpkin_count)
+
+func _input(_event):
+	if Input.is_key_pressed(KEY_ENTER):
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
+	if Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
