1cc5a5a4e4effa1855b5a120297f55574d2d96e4
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-23 23:03:02 +0300

    Artwork polish for main menu and game over screens

--- halloween-game/scripts/main_menu.gd
+++ halloween-game/scripts/main_menu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	$VBoxContainer/Label2.text = "High score: " + str(PumpkinCount.pumpkin_count)
 
 func _input(_event):
-	if Input.is_key_pressed(KEY_ENTER):
+	if Input.is_key_pressed(KEY_E):
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
 	if Input.is_key_pressed(KEY_Q):
 		get_tree().quit()
