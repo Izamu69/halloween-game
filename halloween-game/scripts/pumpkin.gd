f70a7fcda34cd855fd7951ae42e3be2cb99d296f
Izamu <68143836+Izamu69@users.noreply.github.com>
2025-10-25 16:22:46 +0300

    Added pickup sound and music for 3 screens

--- halloween-game/scripts/pumpkin.gd
+++ halloween-game/scripts/pumpkin.gd
@@ -4,4 +4,6 @@ extends StaticBody2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == player:
+		$AudioStreamPlayer2D.playing = true
+		await get_tree().create_timer(0.1).timeout
 		queue_free()
